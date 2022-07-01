bc64f73fca63842cc81bf0343d60d6448c49a382
Valeria Guevara <82094790+ValeriaGuevara1901@users.noreply.github.com>
2022-06-30 22:53:46 -0300

    Add files via upload

---
+++ PrimeiraVez.java
@@ -0,0 +1,10 @@
+public class PrimeiraVez {
+
+	public static void main(String[] args) 
+	{
+		// Sempre estive apaixonada por java depois de criar
+		//arco iris nele com a turma de engenharia civil
+		System.out.println("Hello Word quebrando a maldição do programador ksksks VAGP Devinhouse");
+
+	}
+}
